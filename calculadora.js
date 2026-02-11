982aea3bdc84bcc42152db598cf6a233970ee904
Jose Luis Jimenez <jljasul@gmail.com>
2026-02-11 20:32:05 +0100

    agregada funcion multiplicar

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,11 @@
 // Calculadora simple
 function sumar(a, b) {
-  return a + b;
+    return a + b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
 }
 
 console.log("5 + 3 =", sumar(5, 3));
+console.log("5 * 3 =", multiplicar(5, 3));
